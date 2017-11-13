c59d2105287e39b2d4ac688aeb9f007350081140
Fabio Dieguez <fabio.dieguez@gmail.com>
2017-11-13 23:56:50 +0100

    refs #1 Plantilla codigo start.spring.io

---
+++ src/main/java/com/fdiegz/viticultura/ViticulturaApplication.java
@@ -0,0 +1,12 @@
+package com.fdiegz.viticultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViticulturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViticulturaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fdiegz/viticultura/ViticulturaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fdiegz.viticultura;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViticulturaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
